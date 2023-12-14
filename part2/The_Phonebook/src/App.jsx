f2c6419b9fd38fe7a58bad54fbea3c3fbae4889a
Yisaaaa <ryansanisit19@gmail.com>
2023-12-14 09:14:20 +0800

    done doing The Phonebook

--- part2/The_Phonebook/src/App.jsx
+++ part2/The_Phonebook/src/App.jsx
@@ -1,34 +1,92 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState("")
+  const [filterName, setFilter] = useState("")
+
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    if (nameAlreadyExists()) {
+      alert(`${newName} is already added to phonebook`)
+      return
+    }
+    setPersons(prev => [...prev, { name: newName, number: newNumber }])
+    setNewName("")
+    setNewNumber("")
+  }
+
+  function nameAlreadyExists() {
+    for (let person of persons) {
+      if (person.name === newName) {
+        return true
+      }
+    }
+    return false
+  }
+
+  const filteredNumbers = persons.filter(person => person.name.toLowerCase().startsWith(filterName))
+  const filteredNumbersElements = filteredNumbers.map(person => <p key={person.name} className="text-lg">{person.name} {person.number}</p>)
+
 
   return (
-    <>
+    <div className="flex gap-4 flex-col text-lg p-9">
+      <h1 className="text-4xl text-slate-900 font-extrabold">Phonebook</h1>
+
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <p>Filter shown with a:</p>
+        <input
+          className="border-2 border-slate-700 px-2"
+          value={filterName}
+          onChange={(e) => setFilter(e.target.value)} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+
+      <h2 className="text-3xl font-semibold">Add a new</h2>
+      <form className="flex gap-0 flex-col " onSubmit={handleSubmit}>
+        <div className="mb-3 flex flex-col gap-2">
+          <div className="">
+            <span className="font-semibold">name:</span> <input
+              required
+              value={newName}
+              onChange={(e) => setNewName(e.target.value)}
+              className="border-2 border-gray-600 " />
+          </div>
+          <div>
+            <span className="font-semibold">number: </span>
+            <input
+              required
+              className="border-2 border-gray-600"
+              value={newNumber}
+              onChange={(e) => setNewNumber(e.target.value)} />
+          </div>
+        </div>
+        <div>
+          <button
+            className="text-white bg-slate-800 px-4 rounded"
+            type="submit">add</button>
+        </div>
+      </form>
+      <div className="mb-8">debug: {newName} {newNumber}</div>
+      <h2 className="text-3xl font-bold">Numbers</h2>
+
+      <div className="flex flex-col gap-1">
+        {filteredNumbersElements}
+
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
+
+
   )
 }
 
