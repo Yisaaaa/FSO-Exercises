69fb17251c031dc5fd54dfeaafbdcc803190d490
Yisaaaa <ryansanisit19@gmail.com>
2023-12-14 18:59:00 +0800

    part2: fetching data from the server

--- part2/The_Phonebook/src/App.jsx
+++ part2/The_Phonebook/src/App.jsx
@@ -1,93 +1,97 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from "axios"
 
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState("")
-  const [filterName, setFilter] = useState("")
+    const [persons, setPersons] = useState([])
+    const [newName, setNewName] = useState('')
+    const [newNumber, setNewNumber] = useState("")
+    const [filterName, setFilter] = useState("")
+
+    useEffect(() => {
+        axios.get("http://localhost:3001/persons")
+            .then(res => {
+                console.log(res.data)
+                setPersons(res.data)
+            })
+    }, [])
 
 
-  function handleSubmit(e) {
-    e.preventDefault()
-    if (nameAlreadyExists()) {
-      alert(`${newName} is already added to phonebook`)
-      return
+    function handleSubmit(e) {
+        e.preventDefault()
+        if (nameAlreadyExists()) {
+            alert(`${newName} is already added to phonebook`)
+            return
+        }
+        setPersons(prev => [...prev, { name: newName, number: newNumber }])
+        setNewName("")
+        setNewNumber("")
     }
-    setPersons(prev => [...prev, { name: newName, number: newNumber }])
-    setNewName("")
-    setNewNumber("")
-  }
 
-  function nameAlreadyExists() {
-    for (let person of persons) {
-      if (person.name === newName) {
-        return true
-      }
+    function nameAlreadyExists() {
+        for (let person of persons) {
+            if (person.name === newName) {
+                return true
+            }
+        }
+        return false
     }
-    return false
-  }
 
-  const filteredNumbers = persons.filter(person => person.name.toLowerCase().startsWith(filterName))
-  const filteredNumbersElements = filteredNumbers.map(person => <p key={person.name} className="text-lg">{person.name} {person.number}</p>)
+    const filteredNumbers = persons.filter(person => person.name.toLowerCase().startsWith(filterName))
+    const filteredNumbersElements = filteredNumbers.map(person => <p key={person.name} className="text-lg">{person.name} {person.number}</p>)
 
 
-  return (
-    <div className="flex gap-4 flex-col text-lg p-9">
-      <h1 className="text-4xl text-slate-900 font-extrabold">Phonebook</h1>
+    return (
+        <div className="flex gap-4 flex-col text-lg p-9">
+            <h1 className="text-4xl text-slate-900 font-extrabold">Phonebook</h1>
 
-      <div>
-        <p>Filter shown with a:</p>
-        <input
-          className="border-2 border-slate-700 px-2"
-          value={filterName}
-          onChange={(e) => setFilter(e.target.value)} />
-      </div>
+            <div>
+                <p>Filter shown with a:</p>
+                <input
+                    className="border-2 border-slate-700 px-2"
+                    value={filterName}
+                    onChange={(e) => setFilter(e.target.value)} />
+            </div>
 
 
-      <h2 className="text-3xl font-semibold">Add a new</h2>
-      <form className="flex gap-0 flex-col " onSubmit={handleSubmit}>
-        <div className="mb-3 flex flex-col gap-2">
-          <div className="">
-            <span className="font-semibold">name:</span> <input
-              required
-              value={newName}
-              onChange={(e) => setNewName(e.target.value)}
-              className="border-2 border-gray-600 " />
-          </div>
-          <div>
-            <span className="font-semibold">number: </span>
-            <input
-              required
-              className="border-2 border-gray-600"
-              value={newNumber}
-              onChange={(e) => setNewNumber(e.target.value)} />
-          </div>
+            <h2 className="text-3xl font-semibold">Add a new</h2>
+            <form className="flex gap-0 flex-col " onSubmit={handleSubmit}>
+                <div className="mb-3 flex flex-col gap-2">
+                    <div className="">
+                        <span className="font-semibold">name:</span> <input
+                            required
+                            value={newName}
+                            onChange={(e) => setNewName(e.target.value)}
+                            className="border-2 border-gray-600 " />
+                    </div>
+                    <div>
+                        <span className="font-semibold">number: </span>
+                        <input
+                            required
+                            className="border-2 border-gray-600"
+                            value={newNumber}
+                            onChange={(e) => setNewNumber(e.target.value)} />
+                    </div>
+                </div>
+                <div>
+                    <button
+                        className="text-white bg-slate-800 px-4 rounded"
+                        type="submit">add</button>
+                </div>
+            </form>
+            <div className="mb-8">debug: {newName} {newNumber}</div>
+            <h2 className="text-3xl font-bold">Numbers</h2>
+
+            <div className="flex flex-col gap-1">
+                {filteredNumbersElements}
+
+            </div>
         </div>
-        <div>
-          <button
-            className="text-white bg-slate-800 px-4 rounded"
-            type="submit">add</button>
-        </div>
-      </form>
-      <div className="mb-8">debug: {newName} {newNumber}</div>
-      <h2 className="text-3xl font-bold">Numbers</h2>
-
-      <div className="flex flex-col gap-1">
-        {filteredNumbersElements}
-
-      </div>
-    </div>
 
 
-  )
+    )
 }
 
 export default App
