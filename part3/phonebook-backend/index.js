584e939cbac7986539366fb3892a2090d2e8d7d9
Yisaaaa <ryansanisit19@gmail.com>
2023-12-17 12:42:55 +0800

    added solution for phonebook backend step 2

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -30,7 +30,18 @@ app.get("/api/persons", (req, res) => {
 	res.json(persons);
 });
 
+app.get("/info", (req, res) => {
+	const timeRequestSent = new Date().toString();
+
+	res.send(
+		`
+            <p>Phonebook has info for ${persons.length} people</p>
+            <p>${timeRequestSent}</p>
+       `
+	);
+});
+
 const PORT = 3001;
 
 app.listen(PORT);
-console.log(`Server started in port ${PORT}`);
+console.log(`\n\n\nServer started in port ${PORT}`);
