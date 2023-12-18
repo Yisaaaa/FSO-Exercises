f6b5ae834407b15a01b2040b7d3abeb2997f0b06
Yisaaaa <ryansanisit19@gmail.com>
2023-12-18 09:43:28 +0800

    updates

--- part2/The_Phonebook/src/service/numbers.js
+++ part2/The_Phonebook/src/service/numbers.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = "http://localhost:3001/persons";
+const baseURL = "http://localhost:3001/api/persons";
 
 function create(newObj) {
 	return axios.post(baseURL, newObj).then((res) => res.data);
