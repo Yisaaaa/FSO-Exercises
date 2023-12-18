3eabbcc587ad36a8aafa8aa325a5bf4ad6a36bb7
Yisaaaa <ryansanisit19@gmail.com>
2023-12-18 09:30:52 +0800

    update

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 
 const app = express();
+
+app.use(cors());
+
 app.use(express.json());
 // app.use(morgan("tiny"));
 // morgan(":method :url :status :res[content-length] - :response-time ms");
@@ -103,7 +107,7 @@ app.post("/api/persons/", (req, res) => {
 	res.json(person);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT);
 console.log(`\n\n\nServer started in port ${PORT}`);
