4be68480c91f502d74e476631849c38b82e893cf
Yisaaaa <ryansanisit19@gmail.com>
2024-07-16 20:43:21 +0800

    Update bmi calculator to accept command line arguments

--- part9/typescript/bmiCalculator.ts
+++ part9/typescript/bmiCalculator.ts
@@ -1,6 +1,28 @@
+import { isNotANumber } from "./utils";
+
+interface BmiValues {
+  height: number;
+  weight: number;
+}
+
+// Parses the cmd arguments to make sure the parameters given
+// are correct.
+const parseArguments = (args: string[]): BmiValues => {
+  if (args.length < 4) {
+    throw new Error("Not enough arguments.");
+  } else if (args.length > 4) {
+    throw new Error("Too many arguments.");
+  } else if (isNotANumber(args[2]) || isNotANumber(args[3])) {
+    throw new Error("Arguments must be numbers.");
+  } else {
+    return { height: Number(args[2]), weight: Number(args[3]) };
+  }
+};
+
 // Params: height in cm, weight in kg
 const calculateBmi = (height: number, weight: number): string => {
   // bmi = mass(kg) / height (m2)
+
   const bmi: number = weight / (height / 100) ** 2;
 
   if (bmi < 15) {
@@ -18,4 +40,13 @@ const calculateBmi = (height: number, weight: number): string => {
   }
 };
 
-console.log(calculateBmi(180, 74));
+// console.log(calculateBmi(180, 74));
+try {
+  const { height, weight } = parseArguments(process.argv);
+  console.log(calculateBmi(height, weight));
+} catch (error: unknown) {
+  if (error instanceof Error) {
+    const errorMsg: string = `You got an error: ${error.message}`;
+    console.log(errorMsg);
+  }
+}
